a31638b086f288e9cd659547535130dab919e85b
zvarych220 <149898601+zvarych220@users.noreply.github.com>
2024-06-02 16:11:15 +0300

    menu-full

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,145 +1,205 @@
 <template>
-    <main>
-      <div class="section">
-        <div class="menu">
-          <div class="menu-pole">
-            <div class="menu-pole-link-about-us" id="app">
-              <nav>
-                <transition name="fade" mode="out-in">
-                  <li class="menu-pole-link-down " v-if="!show" @click="show = !show" key="menu">
-                    <a class="menu-pole-link" href="">Про нас</a>
-                    <img :src="downArrow" class="menu-pole-img">
-                  </li>
-                </transition>
-                <transition name="fade">
-                  <ul v-if="show" class="menu-links">
-                    <li v-for="item in items">{{item}}</li>
-                  </ul>
-                </transition>
-              </nav>
-            </div>
-            <ul class="menu-links">
-              <li><a href="" class="menu-pole-link">Каталог продукції</a></li>
-              <li><a href="" class="menu-pole-link">Оплата і доставка</a></li>
-              <li><a href="" class="menu-pole-link">Партнери</a></li>
-              <li><a href="" class="menu-pole-link">Новини</a></li>
-              <li><a href="" class="menu-pole-link">Контакти</a></li>
-            </ul>
+  <main>
+    <div class="section">
+      <div class="menu">
+        <div class="menu-pole">
+          <div class="menu-pole-link-about-us" id="app">
+            <nav>
+              <i class="menu-pole-link-down" @click="show = !show">
+
+                <a class="menu-pole-link" href="#">Про нас <img :src="downArrow" class="menu-pole-img"></a>
+
+              </i>
+              <transition name="fade">
+                <ul v-if="show" class="sub-menu-links">
+                  <li class="menu-item" v-for="(item, index) in items" :key="index">{{ item }}</li>
+                </ul>
+              </transition>
+            </nav>
           </div>
-          <div class="menu-login">
-            <ul>
-                <li><img :src="groupImage" class="menu-pole-login-img" alt="">
-            <a href="" class="menu-pole-login">Вхід | Реєстрація</a></li>
-        </ul>
-          </div>
+          <ul class="main-menu-links">
+            <li><a href="#" class="menu-pole-link">Каталог продукції</a></li>
+            <li><a href="#" class="menu-pole-link">Оплата і доставка</a></li>
+            <li><a href="#" class="menu-pole-link">Партнери</a></li>
+            <li><a href="#" class="menu-pole-link">Новини</a></li>
+            <li><a href="#" class="menu-pole-link">Контакти</a></li>
+          </ul>
+        </div>
+        <div class="menu-login">
+              
+              <a href="#" class="menu-pole-login"><div class="menu-login-href"><img :src="groupImage" class="menu-pole-login-img" alt="">Вхід | Реєстрація</div></a>
         </div>
       </div>
-    </main>
-  </template>
-  
-  <script>
-  import downArrow from '../assets/down-arrow.png';
-  import groupImage from '../assets/Group.svg';
-  
-  export default {
-    data() {
-      return {
-        downArrow,
-        groupImage,
-        show: false, 
-        items: ['Пункт 1', 'Пункт 2', 'Пункт 3'], 
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
- 
-  .menu {
-  display: flex; 
-  align-items: center; 
-}
-  .menu-pole-link-down{
-    list-style-type: none;
-  }
-  .menu-pole {
+    </div>
+  </main>
+</template>
+
+<script>
+import downArrow from '../assets/down-arrow.png';
+import groupImage from '../assets/Group.svg';
+
+export default {
+  data() {
+    return {
+      downArrow,
+      groupImage,
+      show: false,
+      items: ['Наша команда', 'Партнери', 'Договір'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.menu-login-href{
+  align-items: center;
   display: flex;
-  justify-content: flex-start; 
+}
+.fade-enter-active, .fade-leave-active{
+  transition: opacity .3s ease ;
+}
+
+.fade-enter,
+.fade-leave-to{
+  opacity:0;
+}
+
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.menu-pole-link-down {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.menu-pole {
+  display: flex;
   align-items: center;
   flex-grow: 1;
 }
 
-.section{
-    margin-left: 404px;
-    margin-right: 422px;
+.section {
+  margin-left: 404px;
+  margin-right: 422px;
 }
-  .menu-pole-link-about-us {
-    display: flex; 
-    align-items: center;
-    justify-content: flex-start; 
-    margin-right: 20px; 
-  }
-  
-  .menu-links {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    justify-content: flex-start; 
-  }
-  
-  .menu-links li {
-    margin-right: 20px; 
-  }
-  .menu-login li{
-    list-style-type: none;
-  }
-  .menu-pole-link {
-    font-weight: 300;
-    font-size: 16px;
-    color: #fff;
-    font-family: var(--font-family);
-    text-decoration: none;
-    align-items: center;
-  }
-  
-  .menu-pole-img {
-    padding-left: 4px;
-  }
-  
-  .menu-login {
-    display: flex;
-    align-items: center;
-    padding-left: 20px; 
-    border-left: 1px solid #ccc; 
-  }
-  
-  .menu-pole-login {
-    font-weight: 300;
-    font-size: 16px;
-    color: #fff;
-    font-family: var(--font-family);
-    text-decoration: none;
-    margin-top: 1px;
-  }
-  
-  .menu-pole-login-img {
-    margin-left: 15px;
-    margin-right: 10px;
-  }
-  
-  .menu-pole-link:hover,
-  .menu-pole-login:hover {
-    color: #84BE51; 
-  }
-  
-  main {
-    position:absolute;
-top:0px;
-left:0px;
-width: 100%;
 
+.menu-pole-link-about-us {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-right: 20px;
+  flex-direction: column;
+}
+.menu-item{
+  align-items: center;
+  color: #fff;
+  cursor: pointer;
+  list-style-type: none;;
+
+  transition: all .3s ease;
+}
+.main-menu-links {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  justify-content: space-between;
+  flex-grow: 1;
+  margin: 0;
+}
+
+.main-menu-links li {
+  margin-right: 20px;
+  flex: 1;
+  text-align: center;
+}
+
+.sub-menu-links {
+  display: flex;
+  list-style: none;
+  flex-direction: column;
+  position: fixed; /* Зміна з absolute на fixed */
+  background: #347153;
+  padding: 10px 0;
+  z-index: 1000;
+  width: max-content;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  justify-content: space-between;
+  margin-top:0 ;
+}
+
+.sub-menu-links li {
+  margin-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.menu-login :hover  {
+  background-color: #347153;
+}
+
+.menu-pole-link {
+  font-weight: 300;
+  font-size: 16px;
+  color: #fff;
+  font-family: var(--font-family);
+  text-decoration: none;
+  align-items: center;
+  display: block;
+  padding: 10px 0;
+}
+
+.menu-pole-img {
+  padding-left: 4px;
+}
+
+i {
+  font-style: normal;
+}
+
+.menu-login {
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  border-left: 1px solid #ccc;
+}
+
+.menu-pole-login {
+  font-weight: 300;
+  font-size: 16px;
+  color: #fff;
+  font-family: var(--font-family);
+  text-decoration: none;
+  margin-top: 1px;
+}
+
+.menu-pole-login-img {
+  margin-left: 15px;
+  margin-right: 10px;
+}
+
+.menu-pole li:hover {
+  background-color: #347153;
+}
+
+.menu-pole :hover {
+  color: #84BE51;
+}
+
+.menu-pole-link-down :hover {
+  background-color: #347153;
+}
+
+main {
+  position: relative;
+  top: 0px;
+  left: 0px;
+  width: 100%;
   background: #1e6140;
 }
-
-  </style>
-  
+</style>
